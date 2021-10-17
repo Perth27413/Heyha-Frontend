63536bf5530958bd5fed6026184d65a7b163d1b7
Matupoom Chaiwan <matupoom001@gmail.com>
2021-10-17 21:37:38 +0700

    add product lists component + responsive

---
+++ src/components/Home/Home.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import ProductLists from "./ProductLists.svelte";
+</script>
+
+<main>
+  <div id="home">
+    <ProductLists/>
+  </div>
+</main>
+
+<style lang="scss">
+  @import './Home.scss';
+</style>
---
+++ src/components/Home/ProductLists.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+
+</script>
+
+<main id="productListsMain">
+  <div id="productLists">
+    {#each Array(15) as _, i}
+      <div class="grid-item">
+        <div class="product-tag">แนะนำ</div>
+        <img class="product-image" src="https://img.wongnai.com/p/984x0/2019/05/26/3dc36fd7adf042bbadd4475f622964b1.jpg" alt="">
+        <div class="product-name">สันคอหมูสไลด์</div>
+        <div class="product-price-box">
+          <div class="product-price">200 บาท</div>
+        </div>
+        <hr class="product-line">
+        <div class="product-detail">
+          <button class="product-detail-button">รายละเอียดสินค้า</button>
+        </div>
+      </div>  
+    {/each} 
+    
+  </div>
+</main>
+
+<style lang="scss">
+  @import './ProductLists.scss';
+</style>
