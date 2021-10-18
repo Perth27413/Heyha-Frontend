b32670429a84090e734391afdf54367ffc41a86f
Matupoom Chaiwan <matupoom001@gmail.com>
2021-10-18 13:55:02 +0700

    add promotion component and responsive

--- src/components/Home/Promotion.svelte
+++ src/components/Home/Promotion.svelte
@@ -3,6 +3,19 @@
     <div id="banner1Box">
       <img id="banner1" src="images/banner-1.jpg" alt="">
     </div>
+    <div id="promotionTextBox">
+      <hr class="line">
+      <div id="promotionText">โปรโมชั่น</div>
+      <hr class="line">
+    </div>
+    <div id="banner">
+      <div class="banner-promotion">
+        <img class="promotion-img" src="images/banner-1.jpg" alt="">
+      </div>
+      <div class="banner-promotion">
+        <img class="promotion-img" src="images/banner-1.jpg" alt="">
+      </div>
+    </div>
   </div>
 </main>
 
