f3c717edb01a99a77ef7dabaddd8d263c8be20cb
Matupoom Chaiwan <matupoom001@gmail.com>
2021-10-18 12:19:26 +0700

    add banner promotion

---
+++ src/components/Home/Promotion.svelte
@@ -0,0 +1,11 @@
+<main id="promotionMain">
+  <div id="promotionBox">
+    <div id="banner1Box">
+      <img id="banner1" src="images/banner-1.jpg" alt="">
+    </div>
+  </div>
+</main>
+
+<style lang="scss">
+  @import './Promotion.scss'
+</style>
