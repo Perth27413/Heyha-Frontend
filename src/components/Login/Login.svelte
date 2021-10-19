5c8f39526cf150e2c090c3e6f61f51b860248407
Matupoom Chaiwan <matupoom001@gmail.com>
2021-10-19 18:09:04 +0700

    change style login and register

--- src/components/Login/Login.svelte
+++ src/components/Login/Login.svelte
@@ -3,24 +3,19 @@
 
 <main id="loginMain">
     <div id="loginBox">
-        <label >
-            เข้าสู่ระบบ
-        </label>
+        <label for="login-text" id="loginHeaderText">เข้าสู่ระบบ</label>
         <div class="input-box">
             <i class="fa fa-user input-icon" style="position: absolute" aria-hidden="true"></i>
             <input class="input"  type="text" placeholder="ชื่อผู้ใช้ / อีเมล">
         </div>
         <div class="input-box">
             <i class="fa fa-lock input-icon" style="position: absolute" aria-hidden="true"></i>
-            <input type="text" class="input" placeholder="รหัสผ่าน">
+            <input type="password" class="input" placeholder="รหัสผ่าน">
         </div>
-        
-        <div class="login-button">
-            <label>เข้าสู่ระบบ</label>
-        </div>
+        <button class="login-button">เข้าสู่ระบบ</button>
         <div class="box-text">       
-            <label class="label-login">ยังไม่มีบัญชีผู้ใช้?</label>
-            <label class="label-regis"  > สมัครสมาชิก</label>
+            <label for="login-text" class="label-login">ยังไม่มีบัญชีผู้ใช้?</label>
+            <label for="login-text" class="label-regis"  > สมัครสมาชิก</label>
         </div>
     </div>
 </main>
--- src/components/Register/Register.svelte
+++ src/components/Register/Register.svelte
@@ -3,7 +3,7 @@
 
 <main id="regisMain">
     <div id="regisBox">
-        <label>สมัครสมาชิก</label>
+        <label for="register-text" id="regisHeaderText">สมัครสมาชิก</label>
         <div class="input-box">
             <i class="fa fa-envelope input-icon" style="position: absolute" aria-hidden="true"></i>
             <input class="input"  type="text" placeholder="อีเมล">
@@ -14,7 +14,15 @@
         </div>
         <div class="input-box">
             <i class="fa fa-lock input-icon" style="position: absolute" aria-hidden="true"></i>
-            <input class="input"  type="text" placeholder="รหัสผ่าน">
+            <input class="input"  type="password" placeholder="รหัสผ่าน">
+        </div>
+        <div class="input-box">
+            <i class="fas fa-pen-square input-icon" style="position: absolute" aria-hidden="true"></i>
+            <input class="input"  type="text" placeholder="ชื่อ">
+        </div>
+        <div class="input-box">
+            <i class="fas fa-pen-square input-icon" style="position: absolute" aria-hidden="true"></i>
+            <input class="input"  type="text" placeholder="นามสกุล">
         </div>
         <div class="input-box">
             <i class="fa fa-mobile input-icon" style="position: absolute" aria-hidden="true"></i>
@@ -24,12 +32,10 @@
             <i class="fa fa-address-book input-icon" style="position: absolute" aria-hidden="true"></i>
             <input class="input"  type="text" placeholder="ที่อยู่">
         </div>
-        <div class="regis-button">
-            <label>สมัครสมาชิก</label>
-        </div>
+        <button class="regis-button">สมัครสมาชิก</button>
         <div class="box-text">
-            <label class="label-regis">มีบัญชีผู้ใช้แล้ว?</label>
-            <label class="label-login"> เข้าสู่ระบบ</label>
+            <label for="register-text" class="label-regis">มีบัญชีผู้ใช้แล้ว?</label>
+            <label for="register-text" class="label-login"> เข้าสู่ระบบ</label>
         </div>
     </div>
 
