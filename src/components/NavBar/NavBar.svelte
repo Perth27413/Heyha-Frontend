18e3c30a03eaab843d20d941b365f82c45a6dd99
yafapfap <rocker55598@gmail.com>
2021-10-25 14:41:29 +0700

    getUsername By bang

--- src/components/NavBar/NavBar.svelte
+++ src/components/NavBar/NavBar.svelte
@@ -1,25 +1,49 @@
 <script lang="ts">
-  import { navigate } from "svelte-routing"
-  import { isLogin, getIsLogin, setLogout, getUserDetails } from '../../store/user'
-  import { onMount } from 'svelte';
-import { alertSuccess } from '../../store/notify';
+  import { navigate } from "svelte-routing";
+  import {
+    isLogin,
+    getIsLogin,
+    setLogout,
+    getUserDetails,
+  } from "../../store/user";
+  import { onMount } from "svelte";
+  import { alertSuccess } from "../../store/notify";
+  import CartModel from "../../model/cart/CartModel";
+  import { get } from "../../store/api";
 
-  let loginChecked: boolean = getIsLogin()
+  let loginChecked: boolean = getIsLogin();
+  let cartProducts: Array<CartModel> = [];
 
   onMount(() => {
-    isLogin.subscribe(results => loginChecked = results)
-  })
+    isLogin.subscribe((results) => (loginChecked = results));
+  });
 
-  let isShowLogout: boolean = false
+  onMount(async () => {
+    await getCartProduct();
+  });
+
+  let isShowLogout: boolean = false;
 
   function showLogout() {
-    isShowLogout = !isShowLogout
+    isShowLogout = !isShowLogout;
   }
 
   async function logout(): Promise<void> {
-    setLogout()
-    await alertSuccess('ออกจากระบบสำเร็จ')
-    navigate('/')
+    setLogout();
+    await alertSuccess("ออกจากระบบสำเร็จ");
+    navigate("/");
+  }
+
+  async function getCartProduct(): Promise<void> {
+    try {
+      const response: Array<CartModel> = await get(
+        `/cartById?id=${getUserDetails().id}`
+      );
+      cartProducts = response;
+    } catch (error) {
+      console.error(error);
+      cartProducts = [];
+    }
   }
 </script>
 
@@ -27,57 +51,58 @@ import { alertSuccess } from '../../store/notify';
   <div id="navBar">
     <div id="navBox">
       <div id="logoBox">
-        <div id="logo" on:click={() => navigate('/')}>
-          <i class="fas fa-square" aria-hidden="true"></i>
+        <div id="logo" on:click={() => navigate("/")}>
+          <i class="fas fa-square" aria-hidden="true" />
         </div>
-        <div id="logoText" on:click={() => navigate('/')}>
-          เฮฮาหมูกระทะ
-        </div>
+        <div id="logoText" on:click={() => navigate("/")}>เฮฮาหมูกระทะ</div>
       </div>
       {#if !loginChecked}
         <div id="loginRegisBox">
-          <button class="login-regis-btn" on:click={() => navigate('/register')}>สมัครสมาชิก</button>
-          <button class="login-regis-btn" on:click={() => navigate('/login')}>เข้าสู่ระบบ</button>
+          <button class="login-regis-btn" on:click={() => navigate("/register")}
+            >สมัครสมาชิก</button
+          >
+          <button class="login-regis-btn" on:click={() => navigate("/login")}
+            >เข้าสู่ระบบ</button
+          >
         </div>
       {:else}
         <div id="detailsBox">
           <div id="cartMobile">
             <div id="cartMobileBox">
-            <div id="cartMobileIcon">
-              <i class="fas fa-shopping-cart" aria-hidden="true"></i>
-            </div>
-              <div id="cartMobileCount">
-                1
+              <div id="cartMobileIcon">
+                <i class="fas fa-shopping-cart" aria-hidden="true" />
               </div>
+              <div id="cartMobileCount">1</div>
             </div>
           </div>
-          <div id="cartBtn" on:click={() => navigate(`/user/${getUserDetails().id}/cart`)}>
+          <div
+            id="cartBtn"
+            on:click={() => navigate(`/user/${getUserDetails().id}/cart`)}
+          >
             <div id="cartBtnIcon">
-              <i class="fas fa-shopping-cart" aria-hidden="true"></i>
+              <i class="fas fa-shopping-cart" aria-hidden="true" />
             </div>
-            <div id="cartBtnText">
-              ตะกร้าสินค้า
-            </div>
+            <div id="cartBtnText">ตะกร้าสินค้า</div>
             <div id="cartBtnCount">
               <div id="countBox">
                 <div id="countText">
-                  1
+                  {cartProducts.length}
                 </div>
               </div>
             </div>
           </div>
           <div id="userDetails" on:click={() => showLogout()}>
             <div id="userText">
-              PERTH
+              {getUserDetails().username}
             </div>
             <div id="userIcon">
-              <i class="fas fa-user" aria-hidden="true"></i>
+              <i class="fas fa-user" aria-hidden="true" />
             </div>
           </div>
           {#if isShowLogout}
-          <div id="logOut" on:click={logout}>
-            <label for="" id="logoutText">ออกจากระบบ</label>
-          </div>
+            <div id="logOut" on:click={logout}>
+              <label for="" id="logoutText">ออกจากระบบ</label>
+            </div>
           {/if}
         </div>
       {/if}
@@ -86,5 +111,5 @@ import { alertSuccess } from '../../store/notify';
 </main>
 
 <style lang="scss">
- @import './NavBar.scss';
+  @import "./NavBar.scss";
 </style>
