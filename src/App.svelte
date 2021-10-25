e9d2cb7d06db65084ec3aa8b06ed5a927ebcdd28
Matupoom Chaiwan <matupoom001@gmail.com>
2021-10-25 19:51:07 +0700

    add function payment and order details component

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
 	import Layout from './components/Layout/Layout.svelte'
-	import { Router, Route } from "svelte-routing";
-	import Home from './components/Home/Home.svelte';
-	import Login from './components/Login/Login.svelte';
-	import Register from './components/Register/Register.svelte';
-	import ProductDetails from './components/ProductDetails/ProductDetails.svelte';
-	import { getIsLogin } from './store/user';
-	import Cart from './components/Order/Carts.svelte';
+	import { Router, Route } from "svelte-routing"
+	import Home from './components/Home/Home.svelte'
+	import Login from './components/Login/Login.svelte'
+	import Register from './components/Register/Register.svelte'
+	import ProductDetails from './components/ProductDetails/ProductDetails.svelte'
+	import { getIsLogin } from './store/user'
+	import Cart from './components/Order/Carts.svelte'
 	import ConfirmOrder from './components/Order/ConfirmOrder.svelte'
+	import OrderDetails from './components/Order/OrderDetails.svelte'
 
 	function animateTitle(): void {
-		document.title = document.title.substring(1)+document.title.substring(0,1);
-    setTimeout(animateTitle, 150);
+		document.title = document.title.substring(1)+document.title.substring(0,1)
+    setTimeout(animateTitle, 150)
 	}
 	animateTitle()
 	
@@ -26,6 +27,7 @@
 			<Route path="/register"><svelte:component this={Layout} Component={Register} isLogin={getIsLogin()}/></Route>
 			<Route path="/user/:id/cart" let:params><svelte:component this={Layout} Component={Cart} params={params} isLogin={getIsLogin()}/></Route>
 			<Route path="/user/:id/confirm"><svelte:component this={Layout} Component={ConfirmOrder}/></Route>
+			<Route path="/user/:id/order/:orderId" let:params><svelte:component this={Layout} Component={OrderDetails} params={params}/></Route>
 			<Route path="*"><svelte:component this={Layout} Component={Home}/></Route>
 		</div>
 	</Router>
